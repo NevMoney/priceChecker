1cbb65a2d53e3d01a7ca54f762ee4e1d1c42b914
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-25 11:20:50 -0400

    price checker created
    function to check price on specific amazon url and to send an email if the price is below our desired price

---
+++ parser.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const nightmare = require('nightmare')()
+
+const inputs = process.argv.slice(2)
+const url = inputs[0]
+const myPrice = parseFloat(inputs[1])
+const myAddress = inputs[2]
+
+async function checkPrice() {
+  try {
+    const priceString = await nightmare
+      .goto(url)
+      .wait('#priceblock_dealprice')
+      .evaluate(() => document.getElementById('priceblock_dealprice').innerText)
+      .end()
+
+    const price = parseFloat(priceString.replace('$', ''))
+    console.log('price', price)
+
+    if (price < myPrice) {
+      await sendEmail(
+        'Price fell!',
+        `The price on ${url} has dropped below your requested price of $${myPrice} and is currently selling for $${price}.`,
+        myAddress,
+      )
+      console.log('Buy!')
+    }
+  } catch (error) {
+    await sendEmail('Price Checker ERROR', error.message)
+    throw error
+  }
+}
+
+checkPrice()
+
+async function sendEmail(subject, body, myAddress) {
+  const email = {
+    from: 'nev@1dealaway.com',
+    personalizations: [
+      {
+        to: [
+          {
+            email: myAddress,
+          },
+        ],
+      },
+    ],
+    subject: subject,
+    text: body,
+    html: body,
+  }
+  try {
+    let messageSent = await sgMail.send(email)
+    console.log('messageSent', messageSent)
+  } catch (error) {
+    console.log('error', error)
+  }
+}
